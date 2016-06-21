1338aa4c59d88e804c3af8f3dc38101d321309ba
quinnjn <quinn.neumiiller@getbraintree.com>
2016-06-21 15:23:54 -0700

    Add Card to use RelativeLayout, dynamically add card types

--- Drop-In/src/main/java/com/braintreepayments/api/dropin/view/AddCardView.java
+++ Drop-In/src/main/java/com/braintreepayments/api/dropin/view/AddCardView.java
@@ -6,16 +6,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.braintreepayments.api.dropin.R;
 import com.braintreepayments.api.dropin.interfaces.AddPaymentUpdateListener;
 
-public class AddCardView extends LinearLayout {
+import java.util.Arrays;
+import java.util.List;
+
+public class AddCardView extends RelativeLayout {
 
     private EditText mCardNumber;
     private Button mNext;
+    private GridLayout mAcceptedCards;
+
     private AddPaymentUpdateListener mListener;
+    private final List<Integer> validCardTypes = Arrays.asList(
+            R.drawable.bt_visa,
+            R.drawable.bt_mastercard,
+            R.drawable.bt_discover,
+            R.drawable.bt_amex,
+            R.drawable.bt_diners,
+            R.drawable.bt_visa, //TODO UnionPay
+            R.drawable.bt_jcb,
+            R.drawable.bt_maestro
+
+    );
 
     public AddCardView(Context context) {
         super(context);
@@ -36,10 +54,11 @@ public class AddCardView extends LinearLayout {
         if(isInEditMode()) {
             return;
         }
-        setOrientation(VERTICAL);
         LayoutInflater.from(context).inflate(R.layout.bt_add_card, this, true);
         mCardNumber = (EditText)findViewById(R.id.card_number);
         mNext = (Button)findViewById(R.id.next_button);
+        mAcceptedCards = (GridLayout)findViewById(R.id.accepted_cards);
+
         mNext.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +67,16 @@ public class AddCardView extends LinearLayout {
                 }
             }
         });
+        updateValidCardTypes();
+    }
+
+    private void updateValidCardTypes() {
+        mAcceptedCards.removeAllViews();
+        for(int drawable : validCardTypes) {
+            ImageView iv = new ImageView(getContext());
+            iv.setImageDrawable(getResources().getDrawable(drawable));
+            mAcceptedCards.addView(iv);
+        }
     }
 
     public String getNumber() {
