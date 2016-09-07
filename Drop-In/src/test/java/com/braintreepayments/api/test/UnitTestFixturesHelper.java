48e87a451f5179e6da42ca9a9283252494b68a01
brluk and lkorth <code@getbraintree.com>
2016-09-07 13:43:08 -0400

    Remove androidTests

--- Drop-In/src/test/java/com/braintreepayments/api/test/UnitTestFixturesHelper.java
+++ Drop-In/src/test/java/com/braintreepayments/api/test/UnitTestFixturesHelper.java
@@ -1,6 +1,5 @@
 package com.braintreepayments.api.test;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -9,13 +8,14 @@ import static com.braintreepayments.api.test.StringUtils.getStringFromStream;
 public class UnitTestFixturesHelper {
 
     private static final String FIXTURES_PATH = "fixtures/";
-    private static final String LOCAL_UNIT_TEST_FIXTURES_PATH = "src/androidTest/assets/" + FIXTURES_PATH;
 
     public static String stringFromFixture(String filename) {
+
         try {
             InputStream inputStream = null;
             try {
-                inputStream = new FileInputStream(LOCAL_UNIT_TEST_FIXTURES_PATH + filename);
+                inputStream = UnitTestFixturesHelper.class.getClassLoader()
+                        .getResourceAsStream(FIXTURES_PATH + filename);
                 return getStringFromStream(inputStream);
             } finally {
                 if (inputStream != null) {
