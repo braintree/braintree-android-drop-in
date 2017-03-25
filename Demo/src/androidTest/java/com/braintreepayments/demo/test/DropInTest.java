89815a6e368acbfd8c33b763e6af0d78036ec477
lkorth <luke@lukekorth.com>
2017-03-24 23:15:45 -0400

    Improve test reliability

--- Demo/src/androidTest/java/com/braintreepayments/demo/test/DropInTest.java
+++ Demo/src/androidTest/java/com/braintreepayments/demo/test/DropInTest.java
@@ -160,7 +160,7 @@ public class DropInTest extends TestHelper {
     @Test(timeout = 60000)
     public void tokenizesPayPal() {
         uninstallPayPalWallet();
-        onDevice(withText("Add Payment Method")).waitForEnabled().perform(click());
+        onDevice(withText("Add Payment Method")).waitForExists().waitForEnabled().perform(click());
 
         onDevice(withText("PayPal")).perform(click());
         onDevice(withContentDescription("Proceed with Sandbox Purchase")).perform(click());
