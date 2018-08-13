e7c7f09c95f86dd34b2749a2218367efcde497c2
quinnjn <quinn@neumiiller.com>
2018-08-13 15:55:55 -0700

    Remove linear snap helper
    Prevents the recycler view from bouncing back when scrolling to the last
    payment method

--- Drop-In/src/main/java/com/braintreepayments/api/dropin/VaultManagerActivity.java
+++ Drop-In/src/main/java/com/braintreepayments/api/dropin/VaultManagerActivity.java
@@ -9,7 +9,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -67,7 +66,6 @@ public class VaultManagerActivity extends BaseActivity implements PaymentMethodN
 
         mAdapter.setPaymentMethodNonces(nonces);
 
-        new LinearSnapHelper().attachToRecyclerView(vaultManagerView);
         vaultManagerView.setLayoutManager(layoutManager);
         vaultManagerView.addItemDecoration(dividerItemDecoration);
         vaultManagerView.setAdapter(mAdapter);
