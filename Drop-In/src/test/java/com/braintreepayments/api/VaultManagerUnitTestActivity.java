9a7a5b98b567868e3e4b917116ec65da59f4f442
quinnjn <quinn@neumiiller.com>
2018-09-05 14:39:51 -0700

    Properly set a test HTTP client for VaultManager

--- Drop-In/src/test/java/com/braintreepayments/api/VaultManagerUnitTestActivity.java
+++ Drop-In/src/test/java/com/braintreepayments/api/VaultManagerUnitTestActivity.java
@@ -4,42 +4,34 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.braintreepayments.api.dropin.BraintreeUnitTestHttpClient;
-import com.braintreepayments.api.dropin.DropInRequest;
 import com.braintreepayments.api.dropin.R;
 import com.braintreepayments.api.dropin.VaultManagerActivity;
 import com.braintreepayments.api.dropin.adapters.VaultManagerPaymentMethodsAdapter;
 import com.braintreepayments.api.exceptions.InvalidArgumentException;
-import com.braintreepayments.api.interfaces.ConfigurationListener;
 import com.braintreepayments.api.internal.BraintreeGraphQLHttpClient;
 import com.braintreepayments.api.models.PaymentMethodNonce;
 
 import static com.braintreepayments.api.BraintreeFragmentTestUtils.setGraphQlClient;
 import static com.braintreepayments.api.BraintreeFragmentTestUtils.setHttpClient;
-import static com.braintreepayments.api.BraintreeFragmentTestUtils.waitForConfiguration;
-import static com.braintreepayments.api.test.TestTokenizationKey.TOKENIZATION_KEY;
 import static org.mockito.Mockito.spy;
 
 public class VaultManagerUnitTestActivity extends VaultManagerActivity {
 
     public Context context;
     public BraintreeFragment braintreeFragment;
-    public BraintreeUnitTestHttpClient httpClient;
+    public BraintreeUnitTestHttpClient httpClient = new BraintreeUnitTestHttpClient();
     public BraintreeGraphQLHttpClient graphQLHttpClient;
-    public ConfigurationListener configurationListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.bt_vault_manager_activity_theme);
 
-        if (mDropInRequest == null) {
-            mDropInRequest = new DropInRequest().tokenizationKey(TOKENIZATION_KEY);
-        }
+        ConfigurationManagerTestUtils.setFetchingConfiguration(true);
 
         super.onCreate(savedInstanceState);
 
         if (braintreeFragment != null) {
             ConfigurationManagerTestUtils.setFetchingConfiguration(false);
-            waitForConfiguration(braintreeFragment, configurationListener);
         }
     }
 
