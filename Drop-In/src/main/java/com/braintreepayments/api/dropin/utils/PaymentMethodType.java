cc203f91831c04da9b07effce8f70ce6bc466e9f
demerino <code@getbraintree.com>
2019-12-04 11:38:13 -0800

    Update payment option icons.

--- Drop-In/src/main/java/com/braintreepayments/api/dropin/utils/PaymentMethodType.java
+++ Drop-In/src/main/java/com/braintreepayments/api/dropin/utils/PaymentMethodType.java
@@ -25,7 +25,7 @@ public enum PaymentMethodType {
     UNIONPAY(CardType.UNIONPAY.getFrontResource(), R.drawable.bt_ic_vaulted_unionpay, R.string.bt_descriptor_unionpay, "UnionPay", CardType.UNIONPAY),
     HIPER(CardType.HIPER.getFrontResource(), R.drawable.bt_ic_vaulted_hiper, R.string.bt_descriptor_hiper, "Hiper", CardType.HIPER),
     HIPERCARD(CardType.HIPERCARD.getFrontResource(), R.drawable.bt_ic_vaulted_hipercard, R.string.bt_descriptor_hipercard, "Hipercard", CardType.HIPERCARD),
-    UNKNOWN(CardType.UNKNOWN.getFrontResource(), R.drawable.bt_ic_vaulted_unknown, R.string.bt_descriptor_unknown, "Unknown", CardType.UNKNOWN);
+    UNKNOWN(R.drawable.bt_ic_vaulted_unknown, R.drawable.bt_ic_vaulted_unknown, R.string.bt_descriptor_unknown, "Unknown", CardType.UNKNOWN);
 
     private final int mIconDrawable;
     private final int mVaultedDrawable;
