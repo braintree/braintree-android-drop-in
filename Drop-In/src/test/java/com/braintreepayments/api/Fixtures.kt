2c7501db9457507cd502d9addb43a43af9532656
skoop <skoop@paypal.com>
2020-11-06 09:48:05 -0600

    Create Fixtures.kt and stub out fixture names

---
+++ Drop-In/src/test/java/com/braintreepayments/api/Fixtures.kt
@@ -0,0 +1,80 @@
+package com.braintreepayments.api
+
+object Fixtures {
+
+    // language=JSON
+    const val PAYMENT_METHODS_UNIONPAY_CREDIT_CARD = """
+    """
+
+    // language=JSON
+    const val PAYMENT_METHODS_VISA_CREDIT_CARD = """
+    """
+
+    // language=JSON
+    const val RESPONSES_CREDIT_CARD_ERROR_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_CREDIT_CARD_EXPIRATION_ERROR_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_CREDIT_CARD_NON_NUMBER_ERROR_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_GET_PAYMENT_METHODS_EMPTY_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_GET_PAYMENT_METHODS_GOOGLE_PAY_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_GET_PAYMENT_METHODS_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_GET_PAYMENT_METHODS_TWO_CARDS_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_GOOGLE_PAY_NETWORK_TOKENIZED_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_GOOGLE_PAY_NON_NETWORK_TOKENIZED_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_THREE_D_SECURE_LOOKUP_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_UNIONPAY_CAPABILITIES_NOT_SUCCEEDED_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_UNIONPAY_CAPABILITIES_SUCCESS_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_ENROLLMENT_ERROR_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_ENROLLMENT_MOBILE_NUMBER_ERROR_RESPONSE = """
+    """
+
+    // language=JSON
+    const val RESPONSES_UNIONPAY_ENROLLMENT_SMS_NOT_REQUIRED = """
+    """
+
+    // language=JSON
+    const val RESPONSES_VISA_CREDIT_CARD_RESPONSE = """
+    """
+
+    // language=JSON
+    const val CLIENT_TOKEN = """
+    """
+}
