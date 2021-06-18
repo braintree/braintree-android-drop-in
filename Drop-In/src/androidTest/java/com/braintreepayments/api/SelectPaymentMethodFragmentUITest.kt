4f8dc1f5e36a712d92ffd53e9f13c2f73273e08f
Steven <sshropshire@paypal.com>
2021-06-18 10:50:13 -0500

    Add test to check if vaulted payment methods header is displayed.

--- Drop-In/src/androidTest/java/com/braintreepayments/api/SelectPaymentMethodFragmentUITest.kt
+++ Drop-In/src/androidTest/java/com/braintreepayments/api/SelectPaymentMethodFragmentUITest.kt
@@ -15,6 +15,7 @@ import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 import java.util.concurrent.CountDownLatch
 
 // Ref: https://developer.android.com/guide/fragments/test
@@ -84,10 +85,27 @@ class SelectPaymentMethodFragmentUITest {
             fragment.dropInViewModel.setVaultedPaymentMethods(vaultedPaymentMethods)
         }
 
-        onView(withId(R.id.bt_supported_payment_methods_header)).check(matches(isDisplayed()))
+        onView(withId(R.id.bt_vaulted_payment_methods_header)).check(matches(isDisplayed()))
         onView(withText("paypalaccount@example.com")).check(matches(isDisplayed()))
         onView(withText("venmojoe")).check(matches(isDisplayed()))
         onView(withText("1111")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenStateIsRESUMED_whenVaultManagerEnabledAndNoVaultedPaymentMethodsFound_showsNothing() {
+        val dropInRequest = DropInRequest()
+                .vaultManager(true)
+        val bundle = bundleOf("EXTRA_DROP_IN_REQUEST" to dropInRequest)
+
+        val scenario = FragmentScenario.launchInContainer(SelectPaymentMethodFragment::class.java, bundle)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+
+        scenario.onFragment { fragment ->
+            fragment.dropInViewModel.setSupportedPaymentMethods(supportedPaymentMethods)
+            fragment.dropInViewModel.setVaultedPaymentMethods(emptyList())
+        }
+
+        onView(withId(R.id.bt_vaulted_payment_methods_header)).check(matches(not(isDisplayed())))
     }
 
     @Test
@@ -128,100 +146,42 @@ class SelectPaymentMethodFragmentUITest {
 
     @Test(timeout=5000)
     fun whenStateIsRESUMED_whenVaultManagerEnabledAndVaultedCardExists_sendsAnalyticEvent() {
-        val dropInRequest = DropInRequest()
-                .vaultManager(true)
-        val bundle = bundleOf("EXTRA_DROP_IN_REQUEST" to dropInRequest)
+        // TODO: capture all analytics events within a time interval and assert that the target analytics event is emitted
 
-        val scenario = FragmentScenario.launchInContainer(SelectPaymentMethodFragment::class.java, bundle)
-        scenario.moveToState(Lifecycle.State.RESUMED)
-
-        scenario.onFragment { fragment ->
-            val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.PAYMENT_METHODS_VISA_CREDIT_CARD))
-            val vaultedPaymentMethods = listOf(cardNonce)
-
-            fragment.dropInViewModel.setSupportedPaymentMethods(supportedPaymentMethods)
-            fragment.dropInViewModel.setVaultedPaymentMethods(vaultedPaymentMethods)
-
-            val activity = fragment.requireActivity()
-            val fragmentManager = fragment.parentFragmentManager
-
-            fragmentManager.setFragmentResultListener("BRAINTREE_RESULT", activity) { requestKey, result ->
-                val event = result.get("BRAINTREE_EVENT") as DropInAnalyticsEvent
-                assertEquals(event.fragment, "vaulted-card.appear")
-                countDownLatch.countDown()
-            }
-        }
-        countDownLatch.await()
-
-//        String authorization = Fixtures.TOKENIZATION_KEY;
-//        DropInRequest dropInRequest = new DropInRequest().tokenizationKey(authorization);
+//        val dropInRequest = DropInRequest()
+//                .vaultManager(true)
+//        val bundle = bundleOf("EXTRA_DROP_IN_REQUEST" to dropInRequest)
 //
-//        DropInClient dropInClient = new MockDropInClientBuilder()
-//                .build();
-//        setupDropInActivity(authorization, dropInClient, dropInRequest, "sessionId");
-//        mActivityController.setup();
+//        val scenario = FragmentScenario.launchInContainer(SelectPaymentMethodFragment::class.java, bundle)
+//        scenario.moveToState(Lifecycle.State.RESUMED)
 //
-//        CardNonce cardNonce = CardNonce.fromJSON(new JSONObject(Fixtures.PAYMENT_METHODS_VISA_CREDIT_CARD));
-//        PayPalAccountNonce payPalAccountNonce = PayPalAccountNonce.fromJSON(new JSONObject(Fixtures.PAYPAL_ACCOUNT_JSON));
-//        List<PaymentMethodNonce> paymentMethodNonces = Arrays.asList(cardNonce, payPalAccountNonce);
+//        scenario.onFragment { fragment ->
+//            val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.PAYMENT_METHODS_VISA_CREDIT_CARD))
+//            val vaultedPaymentMethods = listOf(cardNonce)
 //
-//        mActivity.showVaultedPaymentMethods(paymentMethodNonces);
-//        verify(dropInClient).sendAnalyticsEvent("vaulted-card.appear");
+//            fragment.dropInViewModel.setSupportedPaymentMethods(supportedPaymentMethods)
+//            fragment.dropInViewModel.setVaultedPaymentMethods(vaultedPaymentMethods)
+//
+//            val activity = fragment.requireActivity()
+//            val fragmentManager = fragment.parentFragmentManager
+//
+//            fragmentManager.setFragmentResultListener("BRAINTREE_RESULT", activity) { requestKey, result ->
+//                val event = result.get("BRAINTREE_EVENT") as DropInAnalyticsEvent
+//                assertEquals(event.fragment, "vaulted-card.appear")
+//                countDownLatch.countDown()
+//            }
+//        }
+//        countDownLatch.await()
     }
 
     @Test
-    fun onCreate_whenVaultedCardDoesNotExist_doesNotSendAnalyticEvent() {
-//        String authorization = Fixtures.TOKENIZATION_KEY;
-//        DropInRequest dropInRequest = new DropInRequest().tokenizationKey(authorization);
-//
-//        DropInClient dropInClient = new MockDropInClientBuilder()
-//                .build();
-//        setupDropInActivity(authorization, dropInClient, dropInRequest, "sessionId");
-//        mActivityController.setup();
-//
-//        PayPalAccountNonce payPalAccountNonce = PayPalAccountNonce.fromJSON(new JSONObject(Fixtures.PAYPAL_ACCOUNT_JSON));
-//        PaymentMethodNonce googlePayCardNonce = GooglePayCardNonce.fromJSON(new JSONObject(Fixtures.GOOGLE_PAY_NETWORK_TOKENIZED_RESPONSE));
-//        List<PaymentMethodNonce> paymentMethodNonces = Arrays.asList(payPalAccountNonce, googlePayCardNonce);
-//
-//
-//        mActivity.showVaultedPaymentMethods(paymentMethodNonces);
-//        verify(dropInClient, never()).sendAnalyticsEvent("vaulted-card.appear");
+    fun whenStateIsRESUMED_whenVaultManagerEnabledAndVaultedCardDoesNotExist_doesNotSendAnalyticEvent() {
+        // TODO: capture all analytics events within a time interval and assert that the target analytics event is emitted
     }
 
     @Test
-    fun onVaultEditButtonClick_sendsAnalyticEvent() {
-//        String authorization = Fixtures.TOKENIZATION_KEY;
-//        DropInRequest dropInRequest = new DropInRequest().tokenizationKey(authorization);
-//
-//        DropInClient dropInClient = new MockDropInClientBuilder()
-//                .getVaultedPaymentMethodsSuccess(new ArrayList<PaymentMethodNonce>())
-//                .build();
-//        setupDropInActivity(authorization, dropInClient, dropInRequest, "sessionId");
-//        mActivityController.setup();
-//
-//        mActivity.onVaultEditButtonClick(null);
-//
-//        verify(dropInClient).sendAnalyticsEvent("manager.appeared");
-    }
-
-    @Test
-    fun onCreate_whenNoVaultedPaymentMethodsFound_showsNothing() {
-//        String authorization = Fixtures.CLIENT_TOKEN;
-//        DropInRequest dropInRequest = new DropInRequest().clientToken(Fixtures.CLIENT_TOKEN);
-//
-//        DropInClient dropInClient = new MockDropInClientBuilder()
-//                .authorization(Authorization.fromString(authorization))
-//                .getConfigurationSuccess(Configuration.fromJson(Fixtures.CONFIGURATION_WITH_GOOGLE_PAY_AND_CARD_AND_PAYPAL))
-//                .getSupportedPaymentMethodsSuccess(new ArrayList<DropInPaymentMethodType>())
-//                .getVaultedPaymentMethodsSuccess(new ArrayList<PaymentMethodNonce>())
-//                .build();
-//        setupDropInActivity(authorization, dropInClient, dropInRequest, "sessionId");
-//        mActivityController.setup();
-//
-//        mActivity.fetchPaymentMethodNonces(true);
-//
-//        assertThat(mActivity.findViewById(R.id.bt_vaulted_payment_methods_wrapper)).isNotShown();
-//        assertThat((TextView) mActivity.findViewById(R.id.bt_supported_payment_methods_header)).hasText(R.string.bt_select_payment_method);
+    fun whenStateIsRESUMED_whenVaultEditButtonClicked_sendsAnalyticsEvent() {
+        // TODO: capture all analytics events within a time interval and assert that the target analytics event is emitted
     }
 
     @Test
