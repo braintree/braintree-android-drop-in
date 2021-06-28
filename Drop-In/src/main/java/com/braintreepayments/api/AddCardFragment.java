1091102c215ce7e41ffc84576515dcb0055cbb22
Steven Shropshire <steven.shropshire@getbraintree.com>
2021-06-28 14:16:51 -0500

    Update style to display card form
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- Drop-In/src/main/java/com/braintreepayments/api/AddCardFragment.java
+++ Drop-In/src/main/java/com/braintreepayments/api/AddCardFragment.java
@@ -12,14 +12,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.braintreepayments.api.dropin.R;
+import com.braintreepayments.cardform.utils.CardType;
+import com.braintreepayments.cardform.view.CardForm;
 
 public class AddCardFragment extends Fragment {
+
+    private CardForm cardForm;
 
     public AddCardFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_card, container, false);
+        cardForm = view.findViewById(R.id.bt_card_form);
+
+        cardForm.getCardEditText().displayCardTypeIcon(false);
+
+        cardForm.cardRequired(true).setup((AppCompatActivity) requireActivity());
+//        mCardForm.setOnCardTypeChangedListener(this);
+//        mCardForm.setOnCardFormValidListener(this);
+//        mCardForm.setOnCardFormSubmitListener(this);
         return view;
     }
 }
