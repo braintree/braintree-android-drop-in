4e59f2c2400f4a46b971184cd2cf5dd2bd0fb48b
Susan Stevens <susan.stevens@getbraintree.com>
2021-06-29 10:49:59 -0500

    Add test to ensure that add card event is sent when submitting a valid card number.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- Drop-In/src/androidTest/java/com/braintreepayments/api/AddCardFragmentUITest.kt
+++ Drop-In/src/androidTest/java/com/braintreepayments/api/AddCardFragmentUITest.kt
@@ -4,21 +4,34 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.braintreepayments.api.CardNumber.VISA
 import com.braintreepayments.cardform.utils.CardType
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.braintreepayments.api.dropin.R
 import org.hamcrest.CoreMatchers.not
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import java.util.concurrent.CountDownLatch
 
 @RunWith(AndroidJUnit4::class)
 class AddCardFragmentUITest {
 
+    private lateinit var countDownLatch: CountDownLatch
+
+    @Before
+    fun beforeEach() {
+        countDownLatch = CountDownLatch(1)
+    }
+
     @Test
     fun whenStateIsRESUMED_sendsAnalyticsEventToNotifyAddCardSelected() {
-        // TODO: assert 'card.selected' analytics event dispatched
+        // TODO: assert 'card.selected' analytics event sent
     }
 
     @Test
@@ -31,7 +44,33 @@ class AddCardFragmentUITest {
         }
 
         onView(isRoot()).perform(waitFor(500))
+        onView(withId(R.id.bt_card_form_card_number)).perform(typeText(VISA))
         onView(withId(R.id.bt_button)).perform(click())
         onView(withId(R.id.bt_button)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun whenStateIsRESUMED_onSubmitButtonClickWithValidCardNumber_sendsAddCardEvent() {
+        val scenario = FragmentScenario.launchInContainer(AddCardFragment::class.java, null, R.style.bt_drop_in_activity_theme)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+
+        scenario.onFragment { fragment ->
+            fragment.dropInViewModel.setSupportedCardTypes(listOf(CardType.VISA))
+        }
+
+        onView(isRoot()).perform(waitFor(500))
+        onView(withId(R.id.bt_card_form_card_number)).perform(typeText(VISA))
+        onView(withId(R.id.bt_button)).perform(click())
+
+        scenario.onFragment { fragment ->
+            val activity = fragment.requireActivity()
+            val fragmentManager = fragment.parentFragmentManager
+            fragmentManager.setFragmentResultListener("BRAINTREE_EVENT", activity) { requestKey, result ->
+                val event = result.get("BRAINTREE_RESULT") as AddCardEvent
+                assertEquals(VISA, event.cardNumber)
+                countDownLatch.countDown()
+            }
+        }
+        countDownLatch.await()
     }
 }
