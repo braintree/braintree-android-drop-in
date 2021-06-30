ded5d528cd51cd61bf8ba8e8adbb5672f49ae29b
skoop <skoop@paypal.com>
2021-06-30 15:58:02 -0500

    Create VaultManagerFragment

---
+++ Drop-In/src/main/java/com/braintreepayments/api/ViewManagerFragment.java
@@ -0,0 +1,30 @@
+package com.braintreepayments.api;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.braintreepayments.api.dropin.R;
+
+public class ViewManagerFragment extends Fragment {
+
+    public ViewManagerFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bt_fragment_view_manager, container, false);
+
+        return view;
+    }
+}
