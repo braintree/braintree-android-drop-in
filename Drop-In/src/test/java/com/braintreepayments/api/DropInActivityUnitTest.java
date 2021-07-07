b0df5f0f02723f33a650bfabdd5a8b6d4ab1d549
Steven <sshropshire@paypal.com>
2021-07-07 15:43:13 -0500

    Remove tests that are no longer relevant.

--- Drop-In/src/test/java/com/braintreepayments/api/DropInActivityUnitTest.java
+++ Drop-In/src/test/java/com/braintreepayments/api/DropInActivityUnitTest.java
@@ -519,24 +519,6 @@ public class DropInActivityUnitTest {
     }
 
     @Test
-    public void onActivityResult_whenVaultManagerResultOk_refreshesVaultedPaymentMethods() {
-        String authorization = Fixtures.TOKENIZATION_KEY;
-        DropInRequest dropInRequest = new DropInRequest()
-                .tokenizationKey(authorization);
-
-        DropInClient dropInClient = new MockDropInClientBuilder()
-                .build();
-        setupDropInActivity(authorization, dropInClient, dropInRequest, "sessionId");
-
-        mActivity.mClientTokenPresent = true;
-        mActivityController.setup();
-
-        mActivity.onActivityResult(2, RESULT_OK, null);
-
-        verify(dropInClient).getVaultedPaymentMethods(same(mActivity), eq(true), any(GetPaymentMethodNoncesCallback.class));
-    }
-
-    @Test
     public void configurationExceptionExitsActivityWithError() {
         assertExceptionIsReturned("configuration-exception",
                 new ConfigurationException("Configuration exception"));
@@ -699,44 +681,6 @@ public class DropInActivityUnitTest {
         assertEquals(0, ((ViewSwitcher) mActivity.findViewById(R.id.bt_loading_view_switcher)).getDisplayedChild());
 
         mActivity.onActivityResult(1, RESULT_CANCELED, null);
-
-        assertEquals(1, ((ViewSwitcher) mActivity.findViewById(R.id.bt_loading_view_switcher)).getDisplayedChild());
-    }
-
-    @Test
-    public void onActivityResult_whenPaymentMethodNonceDeleted_removesLoader() {
-        String authorization = Fixtures.TOKENIZATION_KEY;
-        DropInRequest dropInRequest = new DropInRequest().tokenizationKey(authorization);
-        setupDropInActivity(authorization, mock(DropInClient.class), dropInRequest, "sessionId");
-        mActivityController.setup();
-
-        PayPalAccountNonce paypalNonce = mock(PayPalAccountNonce.class);
-
-        ArrayList<Parcelable> paymentMethodNonces = new ArrayList<>();
-        paymentMethodNonces.add(paypalNonce);
-
-        assertEquals(0, ((ViewSwitcher) mActivity.findViewById(R.id.bt_loading_view_switcher)).getDisplayedChild());
-
-        mActivity.onActivityResult(DELETE_PAYMENT_METHOD_NONCE_CODE, RESULT_OK, new Intent()
-                .putExtra("com.braintreepayments.api.EXTRA_PAYMENT_METHOD_NONCES", paymentMethodNonces));
-
-        assertEquals(1, ((ViewSwitcher) mActivity.findViewById(R.id.bt_loading_view_switcher)).getDisplayedChild());
-    }
-
-    @Test
-    public void onActivityResult_whenEditingFinished_removesLoadingIndicator() {
-        String authorization = Fixtures.TOKENIZATION_KEY;
-        DropInRequest dropInRequest = new DropInRequest().tokenizationKey(authorization);
-
-        DropInClient dropInClient = new MockDropInClientBuilder()
-                .authorization(Authorization.fromString(authorization))
-                .build();
-        setupDropInActivity(authorization, dropInClient, dropInRequest, "sessionId");
-        mActivityController.setup();
-
-        assertEquals(0, ((ViewSwitcher) mActivity.findViewById(R.id.bt_loading_view_switcher)).getDisplayedChild());
-
-        mActivity.onActivityResult(2, RESULT_OK, null);
 
         assertEquals(1, ((ViewSwitcher) mActivity.findViewById(R.id.bt_loading_view_switcher)).getDisplayedChild());
     }
