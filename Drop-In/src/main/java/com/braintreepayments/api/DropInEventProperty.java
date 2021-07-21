1e34f51b1ef23f9cbb9449267d9a7b8b2d0bcb21
Steven <sshropshire@paypal.com>
2021-07-21 16:48:38 -0500

    Re-format existing enums and remove unused enum cases.

--- Drop-In/src/main/java/com/braintreepayments/api/DropInEventProperty.java
+++ Drop-In/src/main/java/com/braintreepayments/api/DropInEventProperty.java
@@ -2,13 +2,10 @@ package com.braintreepayments.api;
 
 public enum DropInEventProperty {
     ANALYTICS_EVENT_NAME("com.braintreepayments.api.DropInEventProperty.SEND_ANALYITCS"),
-    SEND_ANALYITCS("com.braintreepayments.api.DropInEventProperty.SEND_ANALYITCS"),
     CARD_NUMBER("com.braintreepayments.api.DropInEventProperty.CARD_NUMBER"),
-    PAYMENT_METHOD_NONCE("com.braintreepayments.api.DropInEventProperty.PAYMENT_METHOD_NONCE"),
     SUPPORTED_PAYMENT_METHOD_SELECTION("com.braintreepayments.api.DropInEventProperty.SUPPORTED_PAYMENT_METHOD_SELECTION"),
     VAULTED_PAYMENT_METHOD_SELECTION("com.braintreepayments.api.DropInEventProperty.VAULTED_PAYMENT_METHOD_SELECTION"),
-    CARD_DETAILS("com.braintreepayments.api.DropInEventProperty.CARD_DETAILS")
-            ;
+    CARD_DETAILS("com.braintreepayments.api.DropInEventProperty.CARD_DETAILS");
 
     private final String bundleKey;
 
