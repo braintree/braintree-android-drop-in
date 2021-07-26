6b0923e232ef4555758be0697878da89ee34f8be
Steven <sshropshire@paypal.com>
2021-07-26 15:01:10 -0500

    Remove unused imports.

--- Drop-In/src/main/java/com/braintreepayments/api/SupportedPaymentMethodsFragment.java
+++ Drop-In/src/main/java/com/braintreepayments/api/SupportedPaymentMethodsFragment.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.ViewSwitcher;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
