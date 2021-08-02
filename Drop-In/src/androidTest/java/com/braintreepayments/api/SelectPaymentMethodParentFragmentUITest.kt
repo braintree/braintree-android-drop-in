555b4283e73b9b0aa6b6d7b1c85df412aba9d215
Steven <sshropshire@paypal.com>
2021-08-02 10:06:31 -0500

    Add back button press ui test for select payment method fragment.

--- Drop-In/src/androidTest/java/com/braintreepayments/api/SelectPaymentMethodParentFragmentUITest.kt
+++ Drop-In/src/androidTest/java/com/braintreepayments/api/SelectPaymentMethodParentFragmentUITest.kt
@@ -3,15 +3,18 @@ package com.braintreepayments.api
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import junit.framework.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.CountDownLatch
+
+import com.braintreepayments.api.dropin.R
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SelectPaymentMethodParentFragmentUITest {
@@ -69,7 +72,30 @@ class SelectPaymentMethodParentFragmentUITest {
         onView(isRoot()).perform(ViewActions.pressBack())
         onView(isRoot()).perform(waitFor(1000))
 
-        val vaultManagerEvent = events.first { it.type == DropInEventType.CANCEL_DROPIN }
-        assertNotNull(vaultManagerEvent)
+        val cancelDropInEvent = events.first { it.type == DropInEventType.CANCEL_DROPIN }
+        assertNotNull(cancelDropInEvent)
+    }
+
+    @Test
+    fun whenStateIsRESUMED_onBackButtonPress_sendsCancelDropInEvent() {
+        val scenario =
+                FragmentScenario.launchInContainer(SelectPaymentMethodParentFragment::class.java)
+
+        val events = mutableListOf<DropInEvent>()
+
+        scenario.onFragment { fragment ->
+            val activity = fragment.requireActivity()
+            val parentFragmentManager = fragment.parentFragmentManager
+
+            parentFragmentManager.setFragmentResultListener(DropInEvent.REQUEST_KEY, activity) { _, result ->
+                events += DropInEvent.fromBundle(result)
+            }
+        }
+
+        onView(withId(R.id.back_button)).perform(click())
+        onView(isRoot()).perform(waitFor(1000))
+
+        val cancelDropInEvent = events.first { it.type == DropInEventType.CANCEL_DROPIN }
+        assertNotNull(cancelDropInEvent)
     }
 }
