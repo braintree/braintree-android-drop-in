34dee87cb8269a0ad421e5106e5fbc991e03e44e
Steven <sshropshire@paypal.com>
2021-08-03 14:48:57 -0500

    Rename BottomSheetViewModel unit test.

--- Drop-In/src/test/java/com/braintreepayments/api/BottomSheetViewModelUnitTest.java
+++ Drop-In/src/test/java/com/braintreepayments/api/BottomSheetViewModelUnitTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class SelectPaymentMethodChildFragmentListTest {
+public class BottomSheetViewModelUnitTest {
 
     @Test
     public void getItem() {
