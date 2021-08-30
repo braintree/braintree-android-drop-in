692c545634ba87db65fcd1651675b58790e73dcf
sshropshire <58225613+sshropshire@users.noreply.github.com>
2021-08-30 14:04:25 -0500

    Update Drop-In/src/main/java/com/braintreepayments/api/DropInResultCallback.java
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- Drop-In/src/main/java/com/braintreepayments/api/DropInResultCallback.java
+++ Drop-In/src/main/java/com/braintreepayments/api/DropInResultCallback.java
@@ -3,5 +3,9 @@ package com.braintreepayments.api;
 import androidx.annotation.Nullable;
 
 public interface DropInResultCallback {
+    /**
+     * @param dropInResult {@link DropInResult}
+     * @param error an exception that occurred while processing a DropInResult
+     */
     void onResult(@Nullable DropInResult dropInResult, @Nullable Exception error);
 }
