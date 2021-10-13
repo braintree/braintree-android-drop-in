17f10c1139d3e8315387f7d904e7c7f383eddc71
Koop <skoop@paypal.com>
2021-10-13 14:24:41 -0500

    Continue converting unit tests

--- Drop-In/src/test/java/com/braintreepayments/api/DropInActivityUITest.kt
+++ Drop-In/src/test/java/com/braintreepayments/api/DropInActivityUITest.kt
@@ -6,13 +6,16 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.test.platform.app.InstrumentationRegistry
-import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.assertTrue
+import junit.framework.Assert
+import junit.framework.TestCase
+import junit.framework.TestCase.*
+import org.json.JSONException
 import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Matchers
+import org.mockito.Mockito
 import org.mockito.Mockito.*
 import org.robolectric.Robolectric.buildActivity
 import org.robolectric.RobolectricTestRunner
@@ -176,7 +179,7 @@ class DropInActivityUITest {
 
         assertEquals(RESULT_FIRST_USER, shadowActivity.resultCode)
         val exception = shadowActivity.resultIntent
-            .getSerializableExtra(DropInResult.EXTRA_ERROR) as java.lang.Exception?
+            .getSerializableExtra(DropInResult.EXTRA_ERROR) as Exception?
         assertTrue(exception is InvalidArgumentException)
         assertEquals("Tokenization Key or Client Token was invalid.", exception!!.message)
     }
@@ -224,6 +227,137 @@ class DropInActivityUITest {
         assertEquals(RESULT_OK, shadowActivity.resultCode)
         assertEquals(dropInResult, shadowActivity.resultIntent
             .getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT))
+    }
+
+    @Test
+    fun onResume_whenBrowserSwitchReturnsUserCanceledException_setsUserCanceledErrorInViewModel() {
+        val error = UserCanceledException("User canceled 3DS.")
+        val dropInClient = MockDropInClientBuilder()
+            .deliverBrowseSwitchResultError(error)
+            .build()
+
+        setupDropInActivity(dropInClient, dropInRequest)
+
+        assertEquals(error, activity.dropInViewModel.userCanceledError.value)
+    }
+
+    @Test
+    fun onResume_whenBrowserSwitchError_forwardsError() {
+        val error = Exception("A 3DS error")
+        val dropInClient = MockDropInClientBuilder()
+            .deliverBrowseSwitchResultError(error)
+            .build()
+
+        setupDropInActivity(dropInClient, dropInRequest)
+        val shadowActivity = shadowOf(activity)
+
+        verify(dropInClient).sendAnalyticsEvent("sdk.exit.sdk-error")
+        assertEquals(RESULT_FIRST_USER, shadowActivity.resultCode)
+        assertEquals(error, shadowActivity.resultIntent
+            .getSerializableExtra(DropInResult.EXTRA_ERROR))
+        assertTrue(activity.isFinishing)
+    }
+
+    @Test
+    fun onActivityResult_whenDropInResultExists_finishesActivity() {
+        val paymentMethodNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInResult = DropInResult()
+            .paymentMethodNonce(paymentMethodNonce)
+            .deviceData("device data")
+
+        val dropInClient = MockDropInClientBuilder()
+            .handleActivityResultSuccess(dropInResult)
+            .build()
+        setupDropInActivity(dropInClient, dropInRequest)
+        val shadowActivity = shadowOf(activity)
+
+        activity.onActivityResult(BraintreeRequestCodes.THREE_D_SECURE, 1, mock(Intent::class.java))
+        activity.dropInViewModel.setBottomSheetState(BottomSheetState.HIDDEN)
+
+        assertEquals(RESULT_OK, shadowActivity.resultCode)
+        assertEquals(dropInResult, shadowActivity.resultIntent
+            .getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT))
+        assertTrue(activity.isFinishing)
+    }
+
+    @Test
+    fun onActivityResult_whenResultIsUserCanceledException_setsUserCanceledErrorInViewModel() {
+        val error = UserCanceledException("User canceled 3DS.")
+        val dropInClient = MockDropInClientBuilder()
+            .handleActivityResultError(error)
+            .build()
+        setupDropInActivity(dropInClient, dropInRequest)
+
+        activity.onActivityResult(BraintreeRequestCodes.THREE_D_SECURE, 1, mock(Intent::class.java))
+        assertFalse(activity.isFinishing)
+        assertEquals(error, activity.dropInViewModel.userCanceledError.value)
+    }
+
+    @Test
+    fun onActivityResult_whenError_finishesWithError() {
+        val error = Exception("A 3DS error")
+        val dropInClient = MockDropInClientBuilder()
+            .handleActivityResultError(error)
+            .build()
+
+        setupDropInActivity(dropInClient, dropInRequest)
+        val shadowActivity = shadowOf(activity)
+
+        activity.onActivityResult(BraintreeRequestCodes.THREE_D_SECURE, 1, mock(Intent::class.java))
+
+        verify(dropInClient).sendAnalyticsEvent("sdk.exit.sdk-error")
+        assertEquals(RESULT_FIRST_USER, shadowActivity.resultCode)
+        assertEquals(error, shadowActivity.resultIntent.getSerializableExtra(DropInResult.EXTRA_ERROR))
+        assertTrue(activity.isFinishing)
+    }
+
+    @Test
+    fun onResume_sendsAnalyticsEventWhenShown() {
+        val dropInClient = MockDropInClientBuilder()
+            .authorization(authorization)
+            .build()
+
+        setupDropInActivity(dropInClient, dropInRequest)
+
+        verify(dropInClient).sendAnalyticsEvent("appeared")
+    }
+
+    @Test
+    fun onResume_setsIntegrationTypeToDropinForDropinActivity() {
+        setupDropInActivity(mock(DropInClient::class.java), dropInRequest)
+
+        // TODO: revisit integration type metadata and consider passing integration (core PR)
+        // type through BraintreeClient constructor instead of relying on reflection
+//        assertEquals("dropin3", mActivity.getDropInClient().getIntegrationType());
+    }
+
+    @Test
+    fun onResume_onVaultedPaymentMethodSelectedEvent_whenThreeDSecureVerificationFails_reloadsPaymentMethodsAndFinishes() {
+        authorization = Authorization.fromString(UnitTestFixturesHelper.base64EncodedClientTokenFromFixture(Fixtures.CLIENT_TOKEN))
+        val error = Exception("three d secure failure")
+        val dropInClient = MockDropInClientBuilder()
+            .authorization(authorization)
+            .threeDSecureError(error)
+            .shouldPerformThreeDSecureVerification(true)
+            .build()
+
+        val threeDSecureRequest = ThreeDSecureRequest()
+        threeDSecureRequest.amount = "1.00"
+
+        val dropInRequest = DropInRequest()
+        dropInRequest.threeDSecureRequest = threeDSecureRequest
+        dropInRequest.requestThreeDSecureVerification = true
+
+        setupDropInActivity(dropInClient, dropInRequest)
+        val shadowActivity = shadowOf(activity)
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        activity.supportFragmentManager.setFragmentResult(DropInEvent.REQUEST_KEY, DropInEvent.createVaultedPaymentMethodSelectedEvent(cardNonce).toBundle())
+
+        verify(dropInClient).getVaultedPaymentMethods(same(activity), any(GetPaymentMethodNoncesCallback::class.java))
+        assertEquals(RESULT_FIRST_USER, shadowActivity.resultCode)
+        assertEquals(error, shadowActivity.resultIntent.getSerializableExtra(DropInResult.EXTRA_ERROR))
+        assertTrue(activity.isFinishing)
     }
 
     private fun createBrowserSwitchSuccessResult(): BrowserSwitchResult? {
