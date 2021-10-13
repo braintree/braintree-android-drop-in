1819be25d14d30b07eb02550e5e6856abb7f91c5
Koop <skoop@paypal.com>
2021-10-13 09:40:30 -0500

    Fix rebase errors

--- Drop-In/src/androidTest/java/com/braintreepayments/api/DropInActivityUITest.kt
+++ Drop-In/src/androidTest/java/com/braintreepayments/api/DropInActivityUITest.kt
@@ -1,11 +1,14 @@
 package com.braintreepayments.api
 
+import android.content.Context
 import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.json.JSONObject
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Matchers
+import org.mockito.Mockito
 import org.robolectric.Robolectric.buildActivity
 
 @RunWith(AndroidJUnit4::class)
@@ -38,5 +41,208 @@ class DropInActivityUITest {
 
         controller.setup()
         // TODO: figure out how to verify setResult is called on a non-mocked activity
+    }
+
+    @Test
+    fun whenStateIsRESUMED_onFragmentResult_whenAnalyticsEvent_sendsAnalyticsViaDropInClient() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val dropInClient = MockDropInClientBuilder()
+            .authorization(authorization)
+            .build()
+
+        val dropInEvent = DropInEvent.createSendAnalyticsEvent("test-analytics.event")
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+
+        controller.setup()
+        activity.supportFragmentManager.setFragmentResult(DropInEvent.REQUEST_KEY, dropInEvent.toBundle())
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("test-analytics.event")
+    }
+
+    @Test
+    fun whenStateIsRESUMED_onVaultedPaymentMethodSelected_sendsAnalyticsEvent() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val dropInClient = MockDropInClientBuilder()
+            .authorization(authorization)
+            .build()
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+
+        controller.setup()
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInEvent = DropInEvent.createVaultedPaymentMethodSelectedEvent(cardNonce)
+        activity.onVaultedPaymentMethodSelected(dropInEvent)
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("vaulted-card.select")
+    }
+
+    @Test
+    fun whenStateIsRESUMED_removePaymentMethodNonce_whenNonceNotNull_sendsAnalyticsEvent() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInClient = MockDropInClientBuilder()
+            .deletePaymentMethodSuccess(cardNonce)
+            .authorization(authorization)
+            .build()
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+        controller.setup()
+
+        activity.removePaymentMethodNonce(cardNonce)
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("manager.delete.succeeded")
+    }
+
+    @Test
+    fun whenStateIsRESUMED_removePaymentMethodNonce_whenPaymentMethodDeleteException_sendsAnalyticsEvent() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInClient = MockDropInClientBuilder()
+            .deletePaymentMethodError(PaymentMethodDeleteException(cardNonce, Exception("error")))
+            .authorization(authorization)
+            .build()
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+        controller.setup()
+
+        activity.removePaymentMethodNonce(cardNonce)
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("manager.delete.failed")
+    }
+
+    @Test
+    fun whenStateIsRESUMED_removePaymentMethodNonce_whenUnknownError_sendsAnalyticsEvent() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInClient = MockDropInClientBuilder()
+            .deletePaymentMethodError(Exception("error"))
+            .authorization(authorization)
+            .build()
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+        controller.setup()
+
+        activity.removePaymentMethodNonce(cardNonce)
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("manager.unknown.failed")
+    }
+
+    @Test
+    fun whenStateIsRESUMED_onDeletePaymentMethodDropInEvent_whenDialogConfirmed_sendsAnalyticsEvent() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInClient = MockDropInClientBuilder()
+            .authorization(authorization)
+            .build()
+
+        val alertPresenter = Mockito.mock(AlertPresenter::class.java)
+        Mockito.doAnswer { invocation ->
+            val callback = invocation.arguments[2] as DialogInteractionCallback
+            callback.onDialogInteraction(DialogInteraction.POSITIVE)
+        }.`when`(alertPresenter).showConfirmNonceDeletionDialog(
+            Matchers.any(Context::class.java),
+            Matchers.any(PaymentMethodNonce::class.java),
+            Matchers.any(DialogInteractionCallback::class.java)
+        )
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+        controller.setup()
+
+        activity.alertPresenter = alertPresenter
+        activity.onDropInEvent(DropInEvent.createDeleteVaultedPaymentMethodNonceEvent(cardNonce))
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("manager.delete.confirmation.positive")
+    }
+
+    @Test
+    fun whenStateIsRESUMED_onDeletePaymentMethodDropInEvent_whenDialogCancelled_sendsAnalyticsEvent() {
+        val authorization = Authorization.fromString(Fixtures.TOKENIZATION_KEY)
+        val dropInRequest = DropInRequest()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(context, DropInActivity::class.java)
+        intent.putExtra(DropInClient.EXTRA_CHECKOUT_REQUEST, dropInRequest)
+
+        val cardNonce = CardNonce.fromJSON(JSONObject(Fixtures.VISA_CREDIT_CARD_RESPONSE))
+        val dropInClient = MockDropInClientBuilder()
+            .authorization(authorization)
+            .build()
+
+        val alertPresenter = Mockito.mock(AlertPresenter::class.java)
+        Mockito.doAnswer { invocation ->
+            val callback = invocation.arguments[2] as DialogInteractionCallback
+            callback.onDialogInteraction(DialogInteraction.NEGATIVE)
+        }.`when`(alertPresenter).showConfirmNonceDeletionDialog(
+            Matchers.any(Context::class.java),
+            Matchers.any(PaymentMethodNonce::class.java),
+            Matchers.any(DialogInteractionCallback::class.java)
+        )
+
+        val controller = buildActivity(DropInActivity::class.java, intent)
+
+        val activity = controller.get()
+        activity.dropInClient = dropInClient
+        controller.setup()
+
+        activity.alertPresenter = alertPresenter
+        activity.onDropInEvent(DropInEvent.createDeleteVaultedPaymentMethodNonceEvent(cardNonce))
+
+        Mockito.verify(dropInClient).sendAnalyticsEvent("manager.delete.confirmation.negative")
     }
 }
