5f0ed305f9d5be08d0fbbb04642644e157f888e9
sshropshire <58225613+sshropshire@users.noreply.github.com>
2021-11-18 11:07:57 -0600

    Update Drop-In/src/main/java/com/braintreepayments/api/DropInResult.java
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- Drop-In/src/main/java/com/braintreepayments/api/DropInResult.java
+++ Drop-In/src/main/java/com/braintreepayments/api/DropInResult.java
@@ -57,7 +57,7 @@ public class DropInResult implements Parcelable {
 
     /**
      * @return The previously used {@link DropInPaymentMethod} or {@code null} if there was no
-     * previous payment method. If the type is {@link DropInPaymentMethod#GOOGLE_PAY} the Android
+     * previous payment method. If the type is {@link DropInPaymentMethod#GOOGLE_PAY} the Google
      * Pay flow will need to be performed by the user again at the time of checkout,
      * {@link #getPaymentMethodNonce()} will return {@code null} in this case.
      */
