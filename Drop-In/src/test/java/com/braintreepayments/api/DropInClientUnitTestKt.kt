fae0a17a301cd837e824a9e7b9df6e42d627b3d5
sshropshire <sshropshire@paypal.com>
2022-03-30 13:41:48 -0500

    Add TODO.

--- Drop-In/src/test/java/com/braintreepayments/api/DropInClientUnitTestKt.kt
+++ Drop-In/src/test/java/com/braintreepayments/api/DropInClientUnitTestKt.kt
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import java.lang.Exception
 
+// TODO: Slowly migrate the entire DropInClientUnitTest to Kotlin
 @RunWith(RobolectricTestRunner::class)
 class DropInClientUnitTestKt {
 
