fd9be7259b26283d1e201f03cd6e502840a79f75
sshropshire <sshropshire@paypal.com>
2023-07-27 11:57:27 -0500

    Add notice of collection helper.

---
+++ Drop-In/src/main/java/com/braintreepayments/api/NoticeOfCollectionHelper.java
@@ -0,0 +1,26 @@
+package com.braintreepayments.api;
+
+import android.graphics.Color;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class NoticeOfCollectionHelper {
+
+    private NoticeOfCollectionHelper() {}
+
+    static void setNoticeOfCollectionText(TextView textView, String notificeOfCollectionText) {
+        String privacyPolicyUrlString = "https://www.paypal.com/us/legalhub/home?locale.x=en_US";
+        String html = String.format("<a href=\"%s\">%s</a>", privacyPolicyUrlString, notificeOfCollectionText);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            textView.setText(Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            textView.setText(Html.fromHtml(html));
+        }
+
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setLinksClickable(true);
+        textView.setLinkTextColor(Color.BLUE);
+    }
+}
