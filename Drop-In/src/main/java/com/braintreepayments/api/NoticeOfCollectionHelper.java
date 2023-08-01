52791c284f98f3a3a308dd110d4590459d9077f9
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-08-01 15:23:09 -0500

    fix typo in method signature

--- Drop-In/src/main/java/com/braintreepayments/api/NoticeOfCollectionHelper.java
+++ Drop-In/src/main/java/com/braintreepayments/api/NoticeOfCollectionHelper.java
@@ -9,9 +9,9 @@ public class NoticeOfCollectionHelper {
 
     private NoticeOfCollectionHelper() {}
 
-    static void setNoticeOfCollectionText(TextView textView, String notificeOfCollectionText) {
+    static void setNoticeOfCollectionText(TextView textView, String noticeOfCollectionText) {
         String privacyPolicyUrlString = "https://www.paypal.com/us/legalhub/home";
-        String html = String.format("<a href=\"%s\">%s</a>", privacyPolicyUrlString, notificeOfCollectionText);
+        String html = String.format("<a href=\"%s\">%s</a>", privacyPolicyUrlString, noticeOfCollectionText);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             textView.setText(Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT));
