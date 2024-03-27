1a842ce03f5043ecbdc2173c9d5a464781885a35
sshropshire <sshropshire@paypal.com>
2024-03-27 16:05:31 -0500

    Provide documentation for DropInLauncherCallback.

--- Drop-In/src/main/java/com/braintreepayments/api/DropInLauncherCallback.java
+++ Drop-In/src/main/java/com/braintreepayments/api/DropInLauncherCallback.java
@@ -2,8 +2,16 @@ package com.braintreepayments.api;
 
 import androidx.activity.result.ActivityResultCallback;
 
+/**
+ * Callback used to receive an Activity result from {@link DropInLauncher}.
+ */
 public interface DropInLauncherCallback extends ActivityResultCallback<DropInResult> {
 
+    /**
+     * Provides the result of launching {@link DropInActivity}.
+     *
+     * @param result DropInResult
+     */
     @Override
     void onActivityResult(DropInResult result);
 }
