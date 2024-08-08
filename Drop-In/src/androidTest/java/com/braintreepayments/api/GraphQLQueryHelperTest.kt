10e95634d3eed15e0f313308dabfcd6b8ea4633a
Timothy Chow <tichow@paypal.com>
2024-08-08 09:56:17 -0500

    Move GraphQLQueryHelper and StreamHelper from the Core module

---
+++ Drop-In/src/androidTest/java/com/braintreepayments/api/GraphQLQueryHelperTest.kt
@@ -0,0 +1,17 @@
+package com.braintreepayments.api
+
+import android.content.res.Resources.NotFoundException
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.braintreepayments.api.GraphQLQueryHelper.getQuery
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class GraphQLQueryHelperTest {
+
+    @Test(expected = NotFoundException::class)
+    fun query_throwsResourcesNotFoundExceptionForInvalidResources() {
+        getQuery(ApplicationProvider.getApplicationContext(), -1)
+    }
+}
---
+++ Drop-In/src/test/java/com/braintreepayments/api/StreamHelperUnitTest.kt
@@ -0,0 +1,18 @@
+package com.braintreepayments.api
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+
+@RunWith(RobolectricTestRunner::class)
+class StreamHelperUnitTest {
+
+    @Test
+    fun getString_readsAStringFromAStream() {
+        val inputStream = ByteArrayInputStream("Test string".toByteArray(StandardCharsets.UTF_8));
+        assertEquals("Test string", StreamHelper.getString(inputStream))
+    }
+}
